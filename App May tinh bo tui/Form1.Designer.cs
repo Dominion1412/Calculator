b8f60cd24b53f04cd7a413b01119d447a418ea1d
Dominion1412 <33460628+Dominion1412@users.noreply.github.com>
2023-11-22 18:55:50 +0700

    Add files via upload

---
+++ App May tinh bo tui/Form1.Designer.cs
@@ -0,0 +1,426 @@
+﻿namespace App_May_tinh_bo_tui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.symChia = new System.Windows.Forms.Button();
+            this.symNhan = new System.Windows.Forms.Button();
+            this.symTru = new System.Windows.Forms.Button();
+            this.symCong = new System.Windows.Forms.Button();
+            this.symBang = new System.Windows.Forms.Button();
+            this.num0 = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.num7 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.num1 = new System.Windows.Forms.Button();
+            this.Ketqua = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.symC = new System.Windows.Forms.Button();
+            this.symMR = new System.Windows.Forms.Button();
+            this.symXoa = new System.Windows.Forms.Button();
+            this.symCE = new System.Windows.Forms.Button();
+            this.symMC = new System.Windows.Forms.Button();
+            this.symCongtru = new System.Windows.Forms.Button();
+            this.symMS = new System.Windows.Forms.Button();
+            this.symDao = new System.Windows.Forms.Button();
+            this.symPhantram = new System.Windows.Forms.Button();
+            this.symCan = new System.Windows.Forms.Button();
+            this.symMtru = new System.Windows.Forms.Button();
+            this.symMcong = new System.Windows.Forms.Button();
+            this.symPoint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // symChia
+            // 
+            this.symChia.Location = new System.Drawing.Point(200, 165);
+            this.symChia.Name = "symChia";
+            this.symChia.Size = new System.Drawing.Size(35, 35);
+            this.symChia.TabIndex = 35;
+            this.symChia.Text = "/";
+            this.symChia.UseVisualStyleBackColor = true;
+            // 
+            // symNhan
+            // 
+            this.symNhan.Location = new System.Drawing.Point(200, 206);
+            this.symNhan.Name = "symNhan";
+            this.symNhan.Size = new System.Drawing.Size(35, 35);
+            this.symNhan.TabIndex = 34;
+            this.symNhan.Text = "*";
+            this.symNhan.UseVisualStyleBackColor = true;
+            // 
+            // symTru
+            // 
+            this.symTru.Location = new System.Drawing.Point(200, 247);
+            this.symTru.Name = "symTru";
+            this.symTru.Size = new System.Drawing.Size(35, 35);
+            this.symTru.TabIndex = 33;
+            this.symTru.Text = "-";
+            this.symTru.UseVisualStyleBackColor = true;
+            // 
+            // symCong
+            // 
+            this.symCong.Location = new System.Drawing.Point(200, 288);
+            this.symCong.Name = "symCong";
+            this.symCong.Size = new System.Drawing.Size(35, 35);
+            this.symCong.TabIndex = 32;
+            this.symCong.Text = "+";
+            this.symCong.UseVisualStyleBackColor = true;
+            // 
+            // symBang
+            // 
+            this.symBang.Location = new System.Drawing.Point(241, 247);
+            this.symBang.Name = "symBang";
+            this.symBang.Size = new System.Drawing.Size(35, 76);
+            this.symBang.TabIndex = 30;
+            this.symBang.Text = "=";
+            this.symBang.UseVisualStyleBackColor = true;
+            // 
+            // num0
+            // 
+            this.num0.Location = new System.Drawing.Point(77, 288);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(76, 35);
+            this.num0.TabIndex = 29;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = true;
+            this.num0.Click += new System.EventHandler(this.num0_Click);
+            // 
+            // num9
+            // 
+            this.num9.Location = new System.Drawing.Point(159, 165);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(35, 35);
+            this.num9.TabIndex = 28;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = true;
+            this.num9.Click += new System.EventHandler(this.num9_Click);
+            // 
+            // num8
+            // 
+            this.num8.Location = new System.Drawing.Point(118, 165);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(35, 35);
+            this.num8.TabIndex = 27;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = true;
+            this.num8.Click += new System.EventHandler(this.num8_Click);
+            // 
+            // num7
+            // 
+            this.num7.Location = new System.Drawing.Point(77, 165);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(35, 35);
+            this.num7.TabIndex = 26;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = true;
+            this.num7.Click += new System.EventHandler(this.num7_Click);
+            // 
+            // num6
+            // 
+            this.num6.Location = new System.Drawing.Point(159, 206);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(35, 35);
+            this.num6.TabIndex = 25;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = true;
+            this.num6.Click += new System.EventHandler(this.num6_Click);
+            // 
+            // num5
+            // 
+            this.num5.Location = new System.Drawing.Point(118, 206);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(35, 35);
+            this.num5.TabIndex = 24;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = true;
+            this.num5.Click += new System.EventHandler(this.num5_Click);
+            // 
+            // num4
+            // 
+            this.num4.Location = new System.Drawing.Point(77, 206);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(35, 35);
+            this.num4.TabIndex = 23;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = true;
+            this.num4.Click += new System.EventHandler(this.num4_Click);
+            // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(159, 247);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(35, 35);
+            this.num3.TabIndex = 22;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = true;
+            this.num3.Click += new System.EventHandler(this.num3_Click);
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(118, 247);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(35, 35);
+            this.num2.TabIndex = 21;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = true;
+            this.num2.Click += new System.EventHandler(this.num2_Click);
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(77, 247);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(35, 35);
+            this.num1.TabIndex = 20;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            this.num1.Click += new System.EventHandler(this.num1_Click);
+            // 
+            // Ketqua
+            // 
+            this.Ketqua.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Ketqua.Enabled = false;
+            this.Ketqua.Location = new System.Drawing.Point(53, 53);
+            this.Ketqua.Name = "Ketqua";
+            this.Ketqua.Size = new System.Drawing.Size(244, 20);
+            this.Ketqua.TabIndex = 19;
+            this.Ketqua.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.ForeColor = System.Drawing.Color.Red;
+            this.Label1.Location = new System.Drawing.Point(95, 9);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(165, 24);
+            this.Label1.TabIndex = 18;
+            this.Label1.Text = "Máy tính cầm tay";
+            // 
+            // symC
+            // 
+            this.symC.Location = new System.Drawing.Point(159, 124);
+            this.symC.Name = "symC";
+            this.symC.Size = new System.Drawing.Size(35, 35);
+            this.symC.TabIndex = 31;
+            this.symC.Text = "C";
+            this.symC.UseVisualStyleBackColor = true;
+            this.symC.Click += new System.EventHandler(this.symC_Click);
+            // 
+            // symMR
+            // 
+            this.symMR.Location = new System.Drawing.Point(118, 83);
+            this.symMR.Name = "symMR";
+            this.symMR.Size = new System.Drawing.Size(35, 35);
+            this.symMR.TabIndex = 36;
+            this.symMR.Text = "MR";
+            this.symMR.UseVisualStyleBackColor = true;
+            // 
+            // symXoa
+            // 
+            this.symXoa.Location = new System.Drawing.Point(77, 124);
+            this.symXoa.Name = "symXoa";
+            this.symXoa.Size = new System.Drawing.Size(35, 35);
+            this.symXoa.TabIndex = 37;
+            this.symXoa.Text = "←";
+            this.symXoa.UseVisualStyleBackColor = true;
+            this.symXoa.Click += new System.EventHandler(this.symXoa_Click);
+            // 
+            // symCE
+            // 
+            this.symCE.Location = new System.Drawing.Point(118, 124);
+            this.symCE.Name = "symCE";
+            this.symCE.Size = new System.Drawing.Size(35, 35);
+            this.symCE.TabIndex = 38;
+            this.symCE.Text = "CE";
+            this.symCE.UseVisualStyleBackColor = true;
+            this.symCE.Click += new System.EventHandler(this.symCE_Click);
+            // 
+            // symMC
+            // 
+            this.symMC.Location = new System.Drawing.Point(77, 83);
+            this.symMC.Name = "symMC";
+            this.symMC.Size = new System.Drawing.Size(35, 35);
+            this.symMC.TabIndex = 39;
+            this.symMC.Text = "MC";
+            this.symMC.UseVisualStyleBackColor = true;
+            // 
+            // symCongtru
+            // 
+            this.symCongtru.Location = new System.Drawing.Point(200, 124);
+            this.symCongtru.Name = "symCongtru";
+            this.symCongtru.Size = new System.Drawing.Size(35, 35);
+            this.symCongtru.TabIndex = 40;
+            this.symCongtru.Text = "±";
+            this.symCongtru.UseVisualStyleBackColor = true;
+            // 
+            // symMS
+            // 
+            this.symMS.Location = new System.Drawing.Point(159, 83);
+            this.symMS.Name = "symMS";
+            this.symMS.Size = new System.Drawing.Size(35, 35);
+            this.symMS.TabIndex = 41;
+            this.symMS.Text = "MS";
+            this.symMS.UseVisualStyleBackColor = true;
+            // 
+            // symDao
+            // 
+            this.symDao.Location = new System.Drawing.Point(241, 206);
+            this.symDao.Name = "symDao";
+            this.symDao.Size = new System.Drawing.Size(35, 35);
+            this.symDao.TabIndex = 42;
+            this.symDao.Text = "1/x";
+            this.symDao.UseVisualStyleBackColor = true;
+            // 
+            // symPhantram
+            // 
+            this.symPhantram.Location = new System.Drawing.Point(241, 165);
+            this.symPhantram.Name = "symPhantram";
+            this.symPhantram.Size = new System.Drawing.Size(35, 35);
+            this.symPhantram.TabIndex = 43;
+            this.symPhantram.Text = "%";
+            this.symPhantram.UseVisualStyleBackColor = true;
+            // 
+            // symCan
+            // 
+            this.symCan.Location = new System.Drawing.Point(241, 124);
+            this.symCan.Name = "symCan";
+            this.symCan.Size = new System.Drawing.Size(35, 35);
+            this.symCan.TabIndex = 44;
+            this.symCan.Text = "√";
+            this.symCan.UseVisualStyleBackColor = true;
+            // 
+            // symMtru
+            // 
+            this.symMtru.Location = new System.Drawing.Point(241, 83);
+            this.symMtru.Name = "symMtru";
+            this.symMtru.Size = new System.Drawing.Size(35, 35);
+            this.symMtru.TabIndex = 45;
+            this.symMtru.Text = "M-";
+            this.symMtru.UseVisualStyleBackColor = true;
+            // 
+            // symMcong
+            // 
+            this.symMcong.Location = new System.Drawing.Point(200, 83);
+            this.symMcong.Name = "symMcong";
+            this.symMcong.Size = new System.Drawing.Size(35, 35);
+            this.symMcong.TabIndex = 46;
+            this.symMcong.Text = "M+";
+            this.symMcong.UseVisualStyleBackColor = true;
+            // 
+            // symPoint
+            // 
+            this.symPoint.Location = new System.Drawing.Point(159, 288);
+            this.symPoint.Name = "symPoint";
+            this.symPoint.Size = new System.Drawing.Size(35, 35);
+            this.symPoint.TabIndex = 47;
+            this.symPoint.Text = ".";
+            this.symPoint.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 338);
+            this.Controls.Add(this.symPoint);
+            this.Controls.Add(this.symMcong);
+            this.Controls.Add(this.symMtru);
+            this.Controls.Add(this.symCan);
+            this.Controls.Add(this.symPhantram);
+            this.Controls.Add(this.symDao);
+            this.Controls.Add(this.symMS);
+            this.Controls.Add(this.symCongtru);
+            this.Controls.Add(this.symMC);
+            this.Controls.Add(this.symCE);
+            this.Controls.Add(this.symXoa);
+            this.Controls.Add(this.symMR);
+            this.Controls.Add(this.symChia);
+            this.Controls.Add(this.symNhan);
+            this.Controls.Add(this.symTru);
+            this.Controls.Add(this.symCong);
+            this.Controls.Add(this.symC);
+            this.Controls.Add(this.symBang);
+            this.Controls.Add(this.num0);
+            this.Controls.Add(this.num9);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num7);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.Ketqua);
+            this.Controls.Add(this.Label1);
+            this.Name = "Form1";
+            this.Text = "MTCT - Đinh Công Đài";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button symChia;
+        internal System.Windows.Forms.Button symNhan;
+        internal System.Windows.Forms.Button symTru;
+        internal System.Windows.Forms.Button symCong;
+        internal System.Windows.Forms.Button symBang;
+        internal System.Windows.Forms.Button num0;
+        internal System.Windows.Forms.Button num9;
+        internal System.Windows.Forms.Button num8;
+        internal System.Windows.Forms.Button num7;
+        internal System.Windows.Forms.Button num6;
+        internal System.Windows.Forms.Button num5;
+        internal System.Windows.Forms.Button num4;
+        internal System.Windows.Forms.Button num3;
+        internal System.Windows.Forms.Button num2;
+        internal System.Windows.Forms.Button num1;
+        internal System.Windows.Forms.TextBox Ketqua;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button symC;
+        internal System.Windows.Forms.Button symMR;
+        internal System.Windows.Forms.Button symXoa;
+        internal System.Windows.Forms.Button symCE;
+        internal System.Windows.Forms.Button symMC;
+        internal System.Windows.Forms.Button symCongtru;
+        internal System.Windows.Forms.Button symMS;
+        internal System.Windows.Forms.Button symDao;
+        internal System.Windows.Forms.Button symPhantram;
+        internal System.Windows.Forms.Button symCan;
+        internal System.Windows.Forms.Button symMtru;
+        internal System.Windows.Forms.Button symMcong;
+        internal System.Windows.Forms.Button symPoint;
+    }
+}
+
---
+++ App May tinh bo tui/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_May_tinh_bo_tui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void num1_Click(object sender, EventArgs e)
+        {
+            if (Ketqua.Text == "0")
+                Ketqua.Text = "1";
+            else
+                Ketqua.Text = Ketqua.Text + "1";
+        }
+        private void num2_Click(object sender, EventArgs e)
+        {
+
+            if (Ketqua.Text == "0")
+                Ketqua.Text = "2";
+            else
+                Ketqua.Text = Ketqua.Text + "2";
+        }
+        private void num3_Click(object sender, EventArgs e)
+        {
+
+            if (Ketqua.Text == "0")
+                Ketqua.Text = "3";
+            else
+                Ketqua.Text = Ketqua.Text + "3";
+        }
+        private void num4_Click(object sender, EventArgs e)
+        {
+
+            if (Ketqua.Text == "0")
+                Ketqua.Text = "4";
+            else
+                Ketqua.Text = Ketqua.Text + "4";
+        }
+        private void num5_Click(object sender, EventArgs e)
+        {
+
+            if (Ketqua.Text == "0")
+                Ketqua.Text = "5";
+            else
+                Ketqua.Text = Ketqua.Text + "5";
+        }
+        private void num6_Click(object sender, EventArgs e)
+        {
+
+            if (Ketqua.Text == "0")
+                Ketqua.Text = "6";
+            else
+                Ketqua.Text = Ketqua.Text + "6";
+        }
+        private void num7_Click(object sender, EventArgs e)
+        {
+
+            if (Ketqua.Text == "0")
+                Ketqua.Text = "7";
+            else
+                Ketqua.Text = Ketqua.Text + "7";
+        }
+        private void num8_Click(object sender, EventArgs e)
+        {
+
+            if (Ketqua.Text == "0")
+                Ketqua.Text = "8";
+            else
+                Ketqua.Text = Ketqua.Text + "8";
+        }
+        private void num9_Click(object sender, EventArgs e)
+        {
+            if (Ketqua.Text == "0")
+                Ketqua.Text = "9";
+            else
+                Ketqua.Text = Ketqua.Text + "9";
+        }
+        private void num0_Click(object sender, EventArgs e)
+        {
+
+            if (Ketqua.Text == "")
+                Ketqua.Text = "";
+            else
+                Ketqua.Text = Ketqua.Text + "0";
+        }
+        private void symCE_Click(object sender, EventArgs e)
+        {
+            Ketqua.Text = "0";
+        }
+        private void symC_Click(object sender, EventArgs e)
+        {
+            Ketqua.Text = "";
+        }
+
+        private void symXoa_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
